0529550ca80d31d5832dd998e8024b10945976b4
JongDeu <ghk4889@naver.com>
2023-05-03 18:39:09 +0900

    leet-743 refactor

--- leetcode/743_dijkstra.py
+++ leetcode/743_dijkstra.py
@@ -16,32 +16,37 @@ times = [[2, 1, 3], [2, 3, 1], [3, 4, 1]]
 n = 4
 k = 2
 # Output: 3
-times = [[3,5,78],[2,1,1],[1,3,0],[4,3,59],[5,3,85],[5,2,22],[2,4,23],[1,4,43],[4,5,75],[5,1,15],[1,5,91],[4,1,16],[3,2,98],[3,4,22],[5,4,31],[1,2,0],[2,5,4],[4,2,51],[3,1,36],[2,3,59]]
+times = [[3, 5, 78], [2, 1, 1], [1, 3, 0], [4, 3, 59], [5, 3, 85], [5, 2, 22], [2, 4, 23], [1, 4, 43], [4, 5, 75],
+         [5, 1, 15], [1, 5, 91], [4, 1, 16], [3, 2, 98], [3, 4, 22], [5, 4, 31], [1, 2, 0], [2, 5, 4], [4, 2, 51],
+         [3, 1, 36], [2, 3, 59]]
 
-dictimes = collections.defaultdict(list)
+adjacent = collections.defaultdict(list)
 for u, v, w in times:
-    dictimes[u].append([v, w])
+    adjacent[u].append([v, w])
 
-d = collections.defaultdict(list)
+d = collections.defaultdict(list)  # 시작노드(k)부터 특정 노드까지의 현재 거리
 
 d[k] = 0
 minHeap = []
 heapq.heappush(minHeap, (d[k], k))
 
-nums = set(range(1, n+1))
+nums = set(range(1, n + 1))
+
 
 while nums and minHeap:
     d_u, u = heapq.heappop(minHeap)
-    nums.discard(u)     # 노드 하나가 동떨어져 있는 경우: nums는 살아있고, minHeap으로 nums에서 제거된 노드를 또 뽑을 수도 있다.
-    for v, w in dictimes[u]:
+    nums.discard(u) 
+    for v, w in adjacent[u]:
         if v not in nums:
+            # nums에서 제거된 노드를 minHeap에서 또 뽑은 경우
             continue
         elif d[v] == [] or d_u + w < d[v]:
-            d[v] = d_u+w
+            d[v] = d_u + w
             heapq.heappush(minHeap, (d[v], v))
-
+            
+# 노드 하나만 연결 없이 동떨어져 있는 경우엔 해당 노드가 nums에 남아있다.
+# 즉, 모든 노드를 순회할 수 없는 경우이다.
 if nums:
     print(-1)
 
 print(max(d.values()))
-
